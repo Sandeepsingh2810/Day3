125d765f0e3567a2b986f3fe61df0a2f3be82f74
macstudent <sandeepjangra0100@gmail.com>
2019-09-20 18:47:23 -0400

    string function

--- src/main/java/com/lamton/StringHandling.java
+++ src/main/java/com/lamton/StringHandling.java
@@ -1,50 +1,58 @@
 package com.lamton;
 
-import com.sun.deploy.security.SelectableSecurityManager;
+
 
 public class StringHandling {
-    public static void main(String[] args)
-    {
-        String s1="Hello";
-        String s2="Hello";
-        String s3=s1;
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = s1;
         String s4 = new String("Hello");
-        if(s1==s2)
-        {
+        if (s1 == s2) {
             System.out.println("s1==s2");
 
-        }
-        else
-        {
+        } else {
             System.out.println("s1!=s2");
         }
-        if(s3==s2)
-        {
+        if (s3 == s2) {
             System.out.println("s3==s2");
-        }
-        else
-        {
+        } else {
             System.out.println("s3==s2");
         }
 
-        if(s1==s4)
-        {
+        if (s1 == s4) {
             System.out.println("s1==s4");
-        }
-        else
-        {
+        } else {
             System.out.println("s1!=s4");
         }
 
 
-        if(s1.equals(s4))
-        {
+        if (s1.equals(s4)) {
             System.out.println("s1==s4");
-        }
-        else
-        {
+        } else {
             System.out.println("s1!=s4");
         }
+        {
+        System.out.println("s1");
+        s1.toLowerCase();
+        System.out.println("s1");
+        s1.toUpperCase();
+
+
     }
+
+    String collegeName = "Lambton College in Toronto";
+        System.out.println( collegeName.length());
+        System.out.println(collegeName.contains("College"));
+        System.out.println(collegeName.indexOf("Toronto"));
+        System.out.println(collegeName.toUpperCase());
+        System.out.println(collegeName.toLowerCase());
+        System.out.println(collegeName.charAt(10));
+        System.out.println(collegeName.replace("Toronto","New York"));
+        System.out.println(collegeName.subSequence(11,15));
+        System.out.println(collegeName.toCharArray());
+        System.out.println(collegeName.concat("267 Yorkland Blvd.,North York"));
+
 }
-    
+}
+
